123f72b0451a313d144dbfc946a327614da0d0b4
Max-7th-byte <max.dubakov@gmail.com>
2020-03-25 20:58:08 +0100

    Deleted all unrequired parts

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require('path'); // (1) for getting a path to different file on different machines
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express'); // (2) working with all the backend stuff
+const bodyParser = require('body-parser'); // (3)
 
-const app = express();
+const app = express(); // for spinning up the server !!!and all the next executions!!!
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin'); 
+const shopRoutes = require('./routes/shop'); 
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true })); // (3) for parsing a user's information, currently an item ordered
+app.use(express.static(path.join(__dirname, 'public'))); // (4) for detached css files working on the server
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
