b48327ff13c5e9205bd850a5a5da1f9f4b872c8e
Max-7th-byte <max.dubakov@gmail.com>
2020-03-26 17:12:32 +0100

    Turned the whole HTML into a pug

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const rootDir = require('../util/path');
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {docTitle: 'Add Product'});
 });
 
 router.post('/add-product', (req, res, next) => {
