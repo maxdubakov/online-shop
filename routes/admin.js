a4fe493cea63fa4c99d1163958426537e52f2056
Max-7th-byte <max.dubakov@gmail.com>
2020-03-27 06:16:39 +0100

    Turned into Handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -8,11 +8,17 @@ const rootDir = require('../util/path');
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product', 
+        activeAddProduct: true,
+        formsCSS: true,
+        productCSS: true
+    });
 });
 
 router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
