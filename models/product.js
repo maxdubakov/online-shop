8bfe74697d8d26e7a617c7a5e28ff9d587490dfa
Max-7th-byte <max.dubakov@gmail.com>
2020-03-28 08:55:16 +0100

    MVC Model

---
+++ models/product.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+
+// HELPERS
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            cb([]);
+        }else {
+            cb(JSON.parse(data));
+        }
+    });
+}
+//
+
+module.exports = class Product {
+    
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
+        fs.readFile(p, (err, data) => {
+            console.log(err);
+            let products = [];
+            if (!err) {
+                products = JSON.parse(data);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+}
