78e66aad45ed7eb9bd1fac297592a0255ca66a7d
Max-7th-byte <max.dubakov@gmail.com>
2020-03-28 13:23:53 +0100

    Added all the needed ejs pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,7 @@
-const path = require('path');
-
 const express = require('express');
 const router = express.Router();
 
 const productsController = require('../controllers/products');
-
-const products = [];
 
 router.get('/add-product', productsController.getAddProduct);
 
