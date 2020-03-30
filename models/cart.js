4d60186fd0a5bd8e33044e2850e230a235327290
Max-7th-byte <max.dubakov@gmail.com>
2020-03-30 21:19:41 +0200

    Price as number in cart file

--- models/cart.js
+++ models/cart.js
@@ -30,7 +30,7 @@ module.exports = class Cart {
                 cart.products = [...cart.products, updatedProduct];
             }
 
-            cart.totalPrice = cart.totalPrice + productPrice;
+            cart.totalPrice = cart.totalPrice + +productPrice;
             fs.writeFile(p, JSON.stringify(cart), err => {
                 console.log(err);
             });
