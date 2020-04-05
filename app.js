82233a75517fddc5d84dd81e0b039f3cbe44f2b7
Max-7th-byte <max.dubakov@gmail.com>
2020-04-05 12:44:32 +0200

    Details Page Added

--- app.js
+++ app.js
@@ -38,44 +38,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-<<<<<<< Updated upstream
-// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// User.hasMany(Product);
-
-// User.hasOne(Cart);
-// Cart.belongsTo(User);
-
-// Cart.belongsToMany(Product, { through: CartItem });
-// Product.belongsToMany(Cart, { through: CartItem });
-
-// Order.belongsTo(User);
-// User.hasMany(Order);
-
-// Order.belongsToMany(Product, { through: OrderItem});
-// Product.belongsToMany(Order, { through: OrderItem});
-
-sequelize
-    .sync()
-    .then(result => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'Max', email: 'max@gmail.com' });
-        }
-        return user;
-    })
-    .then(user => {
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.error('Error from app.js: ', err);
-    });
-=======
 mongoConnect(() => {
     app.listen(3000);
 });
->>>>>>> Stashed changes
+
